e162e3cf87f06007859632550d1e696b8d68dcee
Kiyoko Saito <kiyoko_saito@Kiyokos-MacBook-Pro.local>
2019-04-06 23:09:48 +0100

    changed the file name of csv

--- faithful.py
+++ faithful.py
@@ -4,7 +4,7 @@
 #calculate mean each column
 
 import numpy
-data = numpy.genfromtxt('data1/faithful.csv',delimiter=',')
+data = numpy.genfromtxt('data/faithful.csv',delimiter=',')
 firstcol = data[:,0]
 meanfirstcol = numpy.mean(data[:,0])
 meansecondcol = numpy.mean(data[:,1])
